831c55c9b04315f71afd95cbf654e902002c1402
harshalbhoyar66 <harshalbhoyarj0b22@gmail.com>
2026-03-02 10:12:12 +0530

    Initial Spring Boot base setup

---
+++ src/main/java/com/cachinglab/caching_lab/CachingLabApplication.java
@@ -0,0 +1,13 @@
+package com.cachinglab.caching_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cachinglab/caching_lab/CachingLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cachinglab.caching_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
